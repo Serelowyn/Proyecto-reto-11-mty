ee35773bdaf577665de61220e3751f05eee9f45b
Memo <fretn655@gmail.com>
2026-07-05 10:48:08 -0600

    se empieza el reto, se hace el espacio para las importaciones, se empieza con pandas para asi poder cargar el csv en cuestion

---
+++ LuisTorres_reto_DS_C4SC4.py
@@ -0,0 +1,8 @@
+# -------------- Importaciones
+
+import pandas as pd
+
+# -------------- Fin de las importaciones Importaciones
+
+""" se carga el csv de datos de empleados del reto"""
+df = pd.read_csv(r"C:\\Users\\sasor\\Desktop\\Tec de mty\\4. Diseño de interfaces visuales ne python\\3. Aplicacion web de ciencia de datos\\reto\\Employee_data.csv")
