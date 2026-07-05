0042cd433f12cf49e78841cf199df41b3ab59df3
Memo <fretn655@gmail.com>
2026-07-05 12:25:44 -0600

    se realiza el titulo y descripcion rapido de la app web pero adicional a esto se reemplazan las comillas por los signos de numero ya que chocan con la visualizacion de la web al momento de correrlo

--- LuisTorres_reto_DS_C4SC4.py
+++ LuisTorres_reto_DS_C4SC4.py
@@ -6,10 +6,10 @@ from matplotlib import pyplot as plt
 
 # -------------- Fin de las importaciones Importaciones
 
-""" se carga el csv de datos de empleados del reto"""
+# se carga el csv de datos de empleados del reto
 df = pd.read_csv(r"C:\\Users\\sasor\\Desktop\\Tec de mty\\4. Diseño de interfaces visuales ne python\\3. Aplicacion web de ciencia de datos\\reto\\Employee_data.csv")
 
-"""empiezo por revisar la integridad de las columnas para verificar si posteriormente necesito hacer una limpieza del df"""
+# empiezo por revisar la integridad de las columnas para verificar si posteriormente necesito hacer una limpieza del df
 
 print(df.head())
 # se detecta error en gender columna
@@ -20,10 +20,13 @@ print(df.dtypes)
 print(df.info)
 print(df.columns)
 
-"""en el caseo de la columna: "gender" se detectaron espacios de mas; se procede a primero pasar a integer la columna del id del manager pero rellenando con un id nuevo que simule la falta de informacion, o sea 0 en este caso. en nonbre de empleado se quitan espacions adicionales y comas."""
+# en el caseo de la columna: "gender" se detectaron espacios de mas; se procede a primero pasar a integer la columna del id del manager pero rellenando con un id nuevo que simule la falta de informacion, o sea 0 en este caso. en nonbre de empleado se quitan espacions adicionales y comas.
 df["manager_id"] = df["manager_id"].fillna(0).astype(int)
 df["gender"] = df["gender"].str.strip()
 
-"""en este caso algunos nombres tienen doble espacio al final, o mas. se aplica el mismo filtro mas veces hasta que quede limpio"""
+# en este caso algunos nombres tienen doble espacio al final, o mas. se aplica el mismo filtro mas veces hasta que quede limpio
 df["name_employee"] = (df["name_employee"].str.replace(",", "", regex=False).str.replace("  ", " ", regex=False).str.replace("  ", " ", regex=False).str.strip())
 
+# titulo y descripcion de la app
+st.title("analisis de desempeno de colaboradores")
+st.write("aplicacion web para el area de marketing de socialize your knowledge. aqui se revisa el desempeno de los colaboradores junto con sus horas trabajadas, salario, edad y estado civil.")
